9c64cc9220774c029ab18834ade9b0b15513a233
Saavik Erman <SaavikErman@gmail.com>
2025-06-14 02:39:20 -0400

    header

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => (
   <section className="home">
-    <h2>Relax. Rejuvenate. Restore.</h2>
+    <h2>Professional Care, Right at Home</h2>
     <p>Professional massage therapy brought directly to your home or office.</p>
   </section>
 );

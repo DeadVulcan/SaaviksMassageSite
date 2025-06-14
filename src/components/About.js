e7f9dc7142416a5242ee4a26e1330445f06117f7
Saavik Erman <SaavikErman@gmail.com>
2025-06-14 00:48:47 -0400

    Bio

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,10 @@ const About = () => (
   <section className="about" id="about">
     <h2>About Erman</h2>
     <p>
-      With over 10 years of experience, Erman brings expert massage therapy
-      directly to your location. He specializes in deep tissue, Swedish, and
-      sports massage.
+    Erman’s Mobile Massage provides expert deep tissue, sports, 
+    myofacial, lymphatic and neuromuscular massage therapy, 
+    delivered directly to your home or office—offering relief, 
+    recovery, and results for busy professionals and active individuals.
     </p>
   </section>
 );

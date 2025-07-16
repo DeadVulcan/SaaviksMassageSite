72acbe947e9096bb3dbb818d1e02fa19ff97a8e8
Saavik Erman <SaavikErman@gmail.com>
2025-07-16 17:40:03 -0400

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import BookingInstructions from "./BookingInstructions";
 
 const Header = () => (
-  <header className="header">
+  <header className="header" data-aos="zoom-in">
     <h1>Erman's Mobile Massage</h1>
     <nav>
       <a
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => (
   <section className="home">
-    <h2>Professional Care, Right at Home</h2>
+    <h2>Professional Massage, Personal Setting</h2>
     <p>Relax, Recover, Reduce Pain. — No Traffic, Only Tranquility.</p>
   </section>
 );

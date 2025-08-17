2d3ad003caab71bd93268884fcfbc42742debd00
Saavik Erman <SaavikErman@gmail.com>
2025-08-17 02:28:29 -0400

    inst

--- src/components/BookingInstructions.js
+++ src/components/BookingInstructions.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const BookingInstructions = () => (
   <p className="booking-instructions">
-    Please include your address information for in-home service.<br />
-    Book online or text 865-724-8275 to check availability
+    Click the "Book Now" button to see services/pricing for online booking.<br />
+    Please text/call 865-724-8275 to inquire about in-home massage.<br />
   </p>
 );
 

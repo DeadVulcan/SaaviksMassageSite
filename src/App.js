825447a9555d761e8f69222fe1291101c730d722
Isaac Farr <isaacgfarr@gmail.com>
2025-08-26 21:21:56 -0400

    first revisions halfway complete. Navigation and landing page reworked

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import Services from './components/Services';
+import Services from "./components/Services";
 import "./App.css";
 import React from "react";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
 import Footer from "./components/Footer";
+import MobileStickyCTA from "./components/MobileStickyCTA";
 
 function App() {
   return (
     <>
       <Header />
+      <div className="main-container">
         <Home />
         <Services />
         <About />
+      </div>
       <Footer />
+      <MobileStickyCTA />
     </>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import MobileStickyCTA from "./MobileStickyCTA";
 
 const Home = () => (
   <section className="home">

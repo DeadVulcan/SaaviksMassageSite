698cb431954c208f7bcdad96e7d8d6d5c927c78d
Saavik Erman <SaavikErman@gmail.com>
2025-09-09 00:19:58 -0400

    address

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,9 @@ const Home = () => (
   <section className="home" id="home">
     <h2>Your wellness, your way — on-site or on location.</h2>
     <p>Relax, Recover, Reduce Pain.</p>
+    <p style={{ fontSize: "20px", fontWeight: "600" }}>
+  1004 West Broadway Ave.
+</p>
   </section>
 );
 
